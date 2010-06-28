b3ac24a60c787a8e545a09b5ca7df9b27db731c0
Diogo Ferreira <diogo@underdev.org>
2010-06-28 23:41:31 +0100

    Cosmetic change of product name

--- device/htc/bravo/vendorsetup.sh
+++ device/htc/bravo/vendorsetup.sh
@@ -20,4 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo full_bravo-userdebug
+add_lunch_combo htc_bravo-eng
+add_lunch_combo htc_bravo-userdebug
